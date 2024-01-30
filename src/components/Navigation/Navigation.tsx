797a3306494eccc3b8345a02bf319a1b1360f678
mayzinei <mze1001web@gmail.com>
2024-01-30 22:39:21 +0630

    fix mobile nav

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,31 +1,57 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import Button from "../ui/Button";
 import { FiMenu } from "react-icons/fi";
+import { GrClose } from "react-icons/gr";
 
 export default function Navigation() {
+	const navLinks = [
+		{ link: "/about", title: "About" },
+		{ link: "/skills", title: "Skills" },
+		{ link: "/projects", title: "Projects" },
+	];
+
+	const [navMenuOpen, setNavMenuOpen] = useState(false);
+
 	return (
-		<div className="absolute top-0 w-full flex items-center justify-between px-10 lg:px-24 py-4">
+		<div
+			className={`absolute top-0 w-full flex items-start lg:items-center justify-between px-5 md:px-14 lg:px-24 py-4 ${
+				navMenuOpen
+					? "bg-bgColor/80 lg:bg-transparent backdrop-blur-md h-screen lg:h-auto z-10 lg:z-0"
+					: ""
+			}`}
+		>
 			<Link href={"/"} className="font-serif text-white text-xl">
 				mayzinei
 			</Link>
-			<div className="group text-xl text-white lg:hidden">
-				<FiMenu />
-			</div>
-			<ul className="flex-col lg:flex-row items-center gap-8 lg:gap-12 text-white hidden group-hover:flex lg:flex">
-				<li className="focus:text-accentColor hover:text-textColor">
-					<Link href={"/about"}>About</Link>
-				</li>
-				<li className="focus:text-accentColor hover:text-textColor">
-					<Link href={"/skills"}>Skills</Link>
-				</li>
-				<li className="focus:text-accentColor hover:text-textColor">
-					<Link href={"/projects"}>Projects</Link>
-				</li>
+
+			<ul
+				className={`lg:flex items-center gap-12 text-white ${
+					navMenuOpen
+						? "flex flex-col lg:flex-row mt-20 lg:mt-0"
+						: "hidden"
+				}`}
+			>
+				{navLinks.map((navLink, index) => (
+					<li
+						key={index}
+						className="text-lg lg:text-base focus:text-accentColor hover:text-textColor"
+					>
+						<Link href={navLink.link}>{navLink.title}</Link>
+					</li>
+				))}
 				<Button>
 					<Link href={"/contact"}>Contact Me</Link>
 				</Button>
 			</ul>
+
+			<button
+				className="w-[80px] flex justify-end text-xl text-white lg:hidden cursor-pointer"
+				onClick={() => setNavMenuOpen(!navMenuOpen)}
+			>
+				{navMenuOpen ? <GrClose /> : <FiMenu />}
+			</button>
 		</div>
 	);
 }
